9deb2c0c1fc89cc92180f74864c97cd0fd079424
Hyunworld <as4706as@gmail.com>
2021-06-06 15:12:40 +0900

    0606

---
+++ 문제/1000___A+B/1000_SANG.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class test1000 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		int sum = A + B;
+		
+		System.out.println(sum);
+	}
+}
