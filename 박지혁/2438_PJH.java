4ffedf9c23a0e97e0d72fc204bae05ed59d0824a
jihyeokpark <jparkdev777@gmail.com>
2021-06-11 14:09:51 +0900

    20210611

---
+++ 박지혁/2438_PJH.java
@@ -0,0 +1,23 @@
+package prep;
+
+import java.util.Scanner;
+
+public class Quiz2438 {
+	
+	/* 문제:
+	 * 첫째 줄에는 별 1 개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 프로그램을 작성하시오.
+	 */
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= i; j++) { //i번만큼 *을 찍어야 하기 때문에, i번만큼 도는 for문 
+				System.out.print("*");
+			}
+			System.out.println(""); //줄바꿈
+		}
+		
+	}
+}
