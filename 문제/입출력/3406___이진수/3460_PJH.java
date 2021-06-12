b4e4da5c0012da3354b1b58e5595cd6e7500ab31
jihyeokpark <jparkdev777@gmail.com>
2021-06-12 15:15:07 +0900

    20210612

---
+++ 문제/입출력/3406___이진수/3460_PJH.java
@@ -0,0 +1,27 @@
+package group_study;
+
+//공부 하면서 찾은 풀이 입니다ㅎㅎ 
+import java.util.Scanner;
+
+public class Quiz3460 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int t = scan.nextInt();
+		
+		for(int i = 1; i <= t; i++) {
+			
+			int decimal = scan.nextInt();
+			String binary = Integer.toBinaryString(decimal);
+			
+			for(int j = binary.length()-1; j >= 0; j--) {
+				
+				if(binary.charAt(j)=='1') {
+					System.out.print(binary.length()-j-1 + " ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
